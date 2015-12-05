c315db0d20d8c1f681478779d166fa9e86b176d6
Ken Ong <kenming.ong@gmail.com>
2015-12-05 22:47:38 +1100

    Update 2015-12-05T11:47:22.474Z

--- build/js/core.js
+++ build/js/core.js
@@ -1,18 +1,4 @@
-angular.module('track-chat.home', [ 'ui.router','track-chat.common' ]);
-
-(function(app) {
-	app.config(["$stateProvider", function($stateProvider) {
-
-		$stateProvider.state('home', {
-			url : '/home',
-			views : {
-				"contents" : {
-					templateUrl : 'modules/home/templates/home.html'
-				}
-			}
-		});
-	}]);
-})(angular.module('track-chat.home'));
+angular.module('track-chat.common', ['ngMaterial']);
 angular.module('track-chat.dashboard', [ 'ui.router' ]);
 
 (function(app) {
@@ -28,7 +14,21 @@ angular.module('track-chat.dashboard', [ 'ui.router' ]);
 		});
 	}]);
 })(angular.module('track-chat.dashboard'));
-angular.module('track-chat.common', ['ngMaterial']);
+angular.module('track-chat.home', [ 'ui.router','track-chat.common' ]);
+
+(function(app) {
+	app.config(["$stateProvider", function($stateProvider) {
+
+		$stateProvider.state('home', {
+			url : '/home',
+			views : {
+				"contents" : {
+					templateUrl : 'modules/home/templates/home.html'
+				}
+			}
+		});
+	}]);
+})(angular.module('track-chat.home'));
 angular.module('track-chat.login', [ 'ui.router' ]);
 
 (function(app) {
@@ -59,6 +59,21 @@ angular.module('track-chat.ping', [ 'ui.router' ]);
 		});
 	}]);
 })(angular.module('track-chat.ping'));
+angular.module('track-chat.profile', [ 'ui.router' ]);
+
+(function(app) {
+	app.config(["$stateProvider", function($stateProvider) {
+
+		$stateProvider.state('profile', {
+			url : '/profile',
+			views : {
+				"contents" : {
+					templateUrl : 'modules/profile/templates/profile.html'
+				}
+			}
+		});
+	}]);
+})(angular.module('track-chat.profile'));
 angular.module('track-chat.register', [ 'ui.router' ]);
 
 (function(app) {
@@ -74,6 +89,41 @@ angular.module('track-chat.register', [ 'ui.router' ]);
 		});
 	}]);
 })(angular.module('track-chat.register'));
+(function(app) {
+
+	app.factory('ResourceFactory', ["$resource", function($resource) {
+		return function(url, params) {
+			params = params || {};
+
+            return $resource(url, params, {
+                'get': {method: 'GET'},
+
+                'save': {method: 'POST'},
+                'create': {method: 'POST'},
+
+                'edit': {method: 'PUT'},
+                'update': {method: 'PUT'},
+
+                'remove': {method: 'DELETE'},
+                'delete': {method: 'DELETE'},
+
+                'list': {method: 'GET', isArray: true},
+                'query': {method: 'GET', isArray: true}
+            });
+		};
+	}]);
+
+})(angular.module('track-chat.common'));
+
+(function(app) {
+	app.controller('DashboardController', ["$scope", function($scope) {
+		$scope.dashboard = '';
+
+	}]);
+})(angular.module('track-chat.dashboard'));
+(function(app) {
+
+})(angular.module('track-chat.dashboard'));
 (function (app) {
     app.controller('HomeController', ["$scope", "home", function ($scope, home) {
         $scope.title = home.title;
@@ -121,41 +171,6 @@ angular.module('track-chat.register', [ 'ui.router' ]);
 	});
 })(angular.module('track-chat.home'));
 (function(app) {
-	app.controller('DashboardController', ["$scope", function($scope) {
-		$scope.dashboard = '';
-
-	}]);
-})(angular.module('track-chat.dashboard'));
-(function(app) {
-
-})(angular.module('track-chat.dashboard'));
-(function(app) {
-
-	app.factory('ResourceFactory', ["$resource", function($resource) {
-		return function(url, params) {
-			params = params || {};
-
-            return $resource(url, params, {
-                'get': {method: 'GET'},
-
-                'save': {method: 'POST'},
-                'create': {method: 'POST'},
-
-                'edit': {method: 'PUT'},
-                'update': {method: 'PUT'},
-
-                'remove': {method: 'DELETE'},
-                'delete': {method: 'DELETE'},
-
-                'list': {method: 'GET', isArray: true},
-                'query': {method: 'GET', isArray: true}
-            });
-		};
-	}]);
-
-})(angular.module('track-chat.common'));
-
-(function(app) {
 	app.controller('LoginController', ["$scope", function($scope) {
 		$scope.login = 'Login';
 
@@ -173,6 +188,41 @@ angular.module('track-chat.register', [ 'ui.router' ]);
 (function(app) {
 
 })(angular.module('track-chat.ping'));
+(function(app) {
+
+})(angular.module('track-chat.profile'));
+(function(app) {
+    app.controller('ProfileController', ['$scope', function($scope) {
+        $scope.profileImg = "img/tracy.jpg";
+        $scope.name = "Tracey Nguyen";
+        $scope.about = "INSERT SOME DESCRIPTION HERE";
+        $scope.skills = [
+            { name: "Advertising" },
+            { name: "Creative Strategy" },
+            { name: "Logo Design" },
+            { name: "Creative Direction" },
+            { name: "Art Direction" },
+            { name: "Graphic Design" },
+            { name: "Brand Development" },
+            { name: "Digital Strategy" },
+            { name: "Integrated Marketing" },
+            { name: "Typography" }
+        ];
+        $scope.tools = [
+            { name: "Spanner" },
+            { name: "Hammer" }
+        ];
+        $scope.projects = [
+            { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop_mobile.png", status: "Help Wanted", name: "Activist Print", owner: "The Andy Warhol Museum",  description: "Activist Print is inspired by the long history of art being used to raise awareness of contemporary issues and inspire change" },
+            { imageUrl: "http://renswijnmalen.nl/bootstrap/mobile.png", status: "In Progress", name: "Radical Renewable Art + Activism Fund", owner: "Ellie Harrison", description: "INSERT DESCRIPTION HERE" },
+            { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop.png", status: "Completed", name: "Coorain Calendar 2016", owner: "Ellie Harrison", description: "INSERT DESCRIPTION HERE" },
+            { imageUrl: "http://renswijnmalen.nl/bootstrap/mobile.png", status: "Completed", name: "Help fund the rebuild of the Delish Glass furnace", owner: "Chelsea and Jeremy Griffith", description: "INSERT DESCRIPTION HERE" }
+        ];
+        $scope.spaces = [
+            { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop_mobile.png", name: "Swinburne University of Technology", description: "This is the venue for the RHoK 2015 summer hackathon", address: "John St", city: "Hawthorn", state: "Victoria", country: "Australia" }
+        ];
+    }]);
+})(angular.module('track-chat.profile'));
 (function (app) {
     app.controller('RegisterController', ["$scope", "$http", function ($scope, $http) {
         $scope.register = 'Register';
@@ -255,7 +305,7 @@ angular.module('track-chat.register', [ 'ui.router' ]);
 })(angular.module('track-chat.register'));
 angular.module('track-chat.3rdparty', ['ngRoute', 'ngResource', 'ui.router', 'ui.bootstrap', 'ngAnimate']);
 
-angular.module('track-chat', ['config', 'track-chat.common', 'track-chat.3rdparty', 'track-chat.home', 'track-chat.ping', 'track-chat.login','track-chat.register', 'track-chat.dashboard']);
+angular.module('track-chat', ['config', 'track-chat.common', 'track-chat.3rdparty', 'track-chat.home', 'track-chat.ping', 'track-chat.login','track-chat.register', 'track-chat.dashboard', 'track-chat.profile']);
 (function(app) {
 	app.config(["$stateProvider", "$urlRouterProvider", function($stateProvider, $urlRouterProvider) {
 		$urlRouterProvider.otherwise('/home');
