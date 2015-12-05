435f63342ac71bd7dc311e71d749d0e22ffb3e1a
Leo <leo.gui@bragnvent.com>
2015-12-06 10:16:58 +1100

    Added list user service

--- modules/dashboard/js/controllers/dashboard.js
+++ modules/dashboard/js/controllers/dashboard.js
@@ -1,5 +1,5 @@
 (function (app) {
-    app.controller('DashboardController', ['$scope', '$location', 'uiGmapGoogleMapApi', 'authService', function ($scope, $location, uiGmapGoogleMapApi, authService) {
+    app.controller('DashboardController', ['$scope', '$location', 'uiGmapGoogleMapApi', 'authService', 'userService', function ($scope, $location, uiGmapGoogleMapApi, authService, userService) {
         authService.checkIfLoginRequired(function() {
             var me = authService.getMyDetails();
             $scope.myUserId = me.objectId;
@@ -9,6 +9,44 @@
             uiGmapGoogleMapApi.then(function (maps) {
                 // alert("Map is ready");
                 console.log(maps);
+            });
+
+            userService.listUsers('').then(function(users) {
+                console.log(users)
+
+                /*
+                $scope.profileImg = (user.photo || {}).url || "/img/user_unknown.png";
+                $scope.name = user.firstName + " " + user.lastName;
+                $scope.isMe = (user.objectId == me.objectId);
+                $scope.about = user.bio;
+                $scope.skills = [
+                    { name: "Advertising" },
+                    { name: "Creative Strategy" },
+                    { name: "Logo Design" },
+                    { name: "Creative Direction" },
+                    { name: "Art Direction" },
+                    { name: "Graphic Design" },
+                    { name: "Brand Development" },
+                    { name: "Digital Strategy" },
+                    { name: "Integrated Marketing" },
+                    { name: "Typography" }
+                ];
+                $scope.tools = [
+                    { name: "Spanner" },
+                    { name: "Hammer" }
+                ];
+                $scope.projects = [
+                    { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop_mobile.png", status: "Help Wanted", name: "Activist Print", owner: "The Andy Warhol Museum",  description: "Activist Print is inspired by the long history of art being used to raise awareness of contemporary issues and inspire change" },
+                    { imageUrl: "http://renswijnmalen.nl/bootstrap/mobile.png", status: "In Progress", name: "Radical Renewable Art + Activism Fund", owner: "Ellie Harrison", description: "INSERT DESCRIPTION HERE" },
+                    { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop.png", status: "Completed", name: "Coorain Calendar 2016", owner: "Ellie Harrison", description: "INSERT DESCRIPTION HERE" },
+                    { imageUrl: "http://renswijnmalen.nl/bootstrap/mobile.png", status: "Completed", name: "Help fund the rebuild of the Delish Glass furnace", owner: "Chelsea and Jeremy Griffith", description: "INSERT DESCRIPTION HERE" }
+                ];
+                $scope.spaces = [
+                    { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop_mobile.png", name: "Swinburne University of Technology", description: "This is the venue for the RHoK 2015 summer hackathon", address: "John St", city: "Hawthorn", state: "Victoria", country: "Australia" }
+                ];
+                */ 
+            }, function(err) {
+                alert("Users not found");
             });
         });
     }]);
