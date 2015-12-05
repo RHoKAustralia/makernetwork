bf0564dc933b47140f0ee011e79aca4c7c246370
Kunal <chinthushamsu@gmail.com>
2015-12-06 10:45:47 +1100

    - Changed the google map t0 angular google map

--- modules/home/js/controllers/home.js
+++ modules/home/js/controllers/home.js
@@ -1,27 +1,7 @@
 (function (app) {
     app.controller('HomeController', function ($scope, home,uiGmapGoogleMapApi) {
         $scope.title = home.title;
-        $scope.works = {
-            seo: {
-                totalclients: 26,
-                keywords: 100,
-                firstpage: 40,
-                secondpage: 36,
-                seoreq: 15,
-                tasks: 25
-            },
-            adwords: {
-                totalclients: 20,
-                active: 15,
-                inactive: 5,
-                tasks: 25
-            },
-            web: {
-                totalclients: 20,
-                active: 15,
-                inactive: 5,
-                tasks: 25
-            }
-        };
+
+        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
     });
 })(angular.module('track-chat.home'));
