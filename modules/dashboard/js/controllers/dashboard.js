8a9245b50625eb6ffb44f55783d87b8c5c811f1c
Kunal <chinthushamsu@gmail.com>
2015-12-06 11:01:48 +1100

    - Removed unnecessary loading of modules, again. - Added google map in dashboard

--- modules/dashboard/js/controllers/dashboard.js
+++ modules/dashboard/js/controllers/dashboard.js
@@ -1,6 +1,9 @@
 (function (app) {
-    app.controller('DashboardController', ['$scope', '$location', 'uiGmapGoogleMapApi', 'authService', 'userService', function ($scope, $location, uiGmapGoogleMapApi, authService, userService) {
-        authService.checkIfLoginRequired(function() {
+    app.controller('DashboardController', function ($scope, $location, uiGmapGoogleMapApi, authService, userService) {
+
+        $scope.map = {center: {latitude: -37.8602828, longitude: 145.079616}, zoom: 8};
+
+        authService.checkIfLoginRequired(function () {
             var me = authService.getMyDetails();
             $scope.myUserId = me.objectId;
             $scope.myFullName = me.firstName + " " + me.lastName;
@@ -11,43 +14,43 @@
                 console.log(maps);
             });
 
-            userService.listUsers('').then(function(users) {
-                console.log(users)
+            userService.listUsers('').then(function (users) {
+                console.log(users);
 
                 /*
-                $scope.profileImg = (user.photo || {}).url || "/img/user_unknown.png";
-                $scope.name = user.firstName + " " + user.lastName;
-                $scope.isMe = (user.objectId == me.objectId);
-                $scope.about = user.bio;
-                $scope.skills = [
-                    { name: "Advertising" },
-                    { name: "Creative Strategy" },
-                    { name: "Logo Design" },
-                    { name: "Creative Direction" },
-                    { name: "Art Direction" },
-                    { name: "Graphic Design" },
-                    { name: "Brand Development" },
-                    { name: "Digital Strategy" },
-                    { name: "Integrated Marketing" },
-                    { name: "Typography" }
-                ];
-                $scope.tools = [
-                    { name: "Spanner" },
-                    { name: "Hammer" }
-                ];
-                $scope.projects = [
-                    { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop_mobile.png", status: "Help Wanted", name: "Activist Print", owner: "The Andy Warhol Museum",  description: "Activist Print is inspired by the long history of art being used to raise awareness of contemporary issues and inspire change" },
-                    { imageUrl: "http://renswijnmalen.nl/bootstrap/mobile.png", status: "In Progress", name: "Radical Renewable Art + Activism Fund", owner: "Ellie Harrison", description: "INSERT DESCRIPTION HERE" },
-                    { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop.png", status: "Completed", name: "Coorain Calendar 2016", owner: "Ellie Harrison", description: "INSERT DESCRIPTION HERE" },
-                    { imageUrl: "http://renswijnmalen.nl/bootstrap/mobile.png", status: "Completed", name: "Help fund the rebuild of the Delish Glass furnace", owner: "Chelsea and Jeremy Griffith", description: "INSERT DESCRIPTION HERE" }
-                ];
-                $scope.spaces = [
-                    { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop_mobile.png", name: "Swinburne University of Technology", description: "This is the venue for the RHoK 2015 summer hackathon", address: "John St", city: "Hawthorn", state: "Victoria", country: "Australia" }
-                ];
-                */ 
-            }, function(err) {
+                 $scope.profileImg = (user.photo || {}).url || "/img/user_unknown.png";
+                 $scope.name = user.firstName + " " + user.lastName;
+                 $scope.isMe = (user.objectId == me.objectId);
+                 $scope.about = user.bio;
+                 $scope.skills = [
+                 { name: "Advertising" },
+                 { name: "Creative Strategy" },
+                 { name: "Logo Design" },
+                 { name: "Creative Direction" },
+                 { name: "Art Direction" },
+                 { name: "Graphic Design" },
+                 { name: "Brand Development" },
+                 { name: "Digital Strategy" },
+                 { name: "Integrated Marketing" },
+                 { name: "Typography" }
+                 ];
+                 $scope.tools = [
+                 { name: "Spanner" },
+                 { name: "Hammer" }
+                 ];
+                 $scope.projects = [
+                 { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop_mobile.png", status: "Help Wanted", name: "Activist Print", owner: "The Andy Warhol Museum",  description: "Activist Print is inspired by the long history of art being used to raise awareness of contemporary issues and inspire change" },
+                 { imageUrl: "http://renswijnmalen.nl/bootstrap/mobile.png", status: "In Progress", name: "Radical Renewable Art + Activism Fund", owner: "Ellie Harrison", description: "INSERT DESCRIPTION HERE" },
+                 { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop.png", status: "Completed", name: "Coorain Calendar 2016", owner: "Ellie Harrison", description: "INSERT DESCRIPTION HERE" },
+                 { imageUrl: "http://renswijnmalen.nl/bootstrap/mobile.png", status: "Completed", name: "Help fund the rebuild of the Delish Glass furnace", owner: "Chelsea and Jeremy Griffith", description: "INSERT DESCRIPTION HERE" }
+                 ];
+                 $scope.spaces = [
+                 { imageUrl: "http://renswijnmalen.nl/bootstrap/desktop_mobile.png", name: "Swinburne University of Technology", description: "This is the venue for the RHoK 2015 summer hackathon", address: "John St", city: "Hawthorn", state: "Victoria", country: "Australia" }
+                 ];
+                 */
+            }, function (err) {
                 alert("Users not found");
             });
         });
-    }]);
+    });
 })(angular.module('track-chat.dashboard'));
--- modules/home/js/controllers/home.js
+++ modules/home/js/controllers/home.js
@@ -2,6 +2,6 @@
     app.controller('HomeController', function ($scope, home,uiGmapGoogleMapApi) {
         $scope.title = home.title;
 
-        $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+        $scope.map = { center: { latitude: -37.8602828, longitude: 145.079616 }, zoom: 8 };
     });
 })(angular.module('track-chat.home'));
